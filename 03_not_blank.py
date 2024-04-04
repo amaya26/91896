7af40061d42d829a7022837b174430c5d7fcc9bb
amaya26 <165841780+amaya26@users.noreply.github.com>
2024-04-05 12:43:28 +1300

    Add files via upload

--- 03_not_blank.py
+++ 03_not_blank.py
@@ -14,9 +14,24 @@ def not_blank(question):
 
 
 # main routine goes here
+tickets_sold = 0
+
 while True:
     name = not_blank("Enter your name (or 'xxx' to quit) ")
     if name == "xxx":
         break
+
+    age = int(input("Age: "))
+
+    if 12 <= age <= 120:
+        pass
+    elif age < 12:
+        print("Sorry you are too young for this movie")
+        continue
+    else:
+        print("?? That looks like a typo, please try again.")
+        continue
+
+    tickets_sold += 1
 
 print("We are done")
