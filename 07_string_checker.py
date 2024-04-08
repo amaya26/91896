21e74833a0c2a4856a01db3b1f61d2aa3a39cf78
amaya26 <165841780+amaya26@users.noreply.github.com>
2024-04-08 14:14:10 +1200

    Add files via upload

---
+++ 07_string_checker.py
@@ -0,0 +1,9 @@
+# Checks that users enter a valid response (eg yes / no
+# cash / credit) based on a list of questions
+def string_checker(question, num_letters, valid_responses):
+    pass
+
+
+# main routine starts here
+yes_no_list = ["yes", "no"]
+payment_list = ["cash", "credit"]
